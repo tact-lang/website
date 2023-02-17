e3addb11433d3609e852cd967ce572589075d737
Steve Korshakov <korshakov.stepan@gmail.com>
2023-02-17 19:55:53 +0400

    Update LINKS.ts

--- src/app/core/constants/LINKS.ts
+++ src/app/core/constants/LINKS.ts
@@ -1,7 +1,7 @@
 export enum LINKS {
   DESIGN = 'https://github.com/tact-lang/docs/blob/main/tact-design.md',
   DOCS = 'https://docs.tact-lang.org',
-  GITHUB = 'https://github.com/ton-core/tact',
+  GITHUB = 'https://github.com/tact-lang/tact',
   TELEGRAM = 'https://t.me/tactlang',
   TON = 'https://ton.org/',
   TWITTER = 'http://twitter.com/tact_language'
