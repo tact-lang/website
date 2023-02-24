799d57b209a0d3da859f50a03ad820b8f219f6ab
Hiyorimi <playittodeath@gmail.com>
2023-02-24 17:51:21 +0400

    updated with 10 commandments

--- src/app/core/constants/LINKS.ts
+++ src/app/core/constants/LINKS.ts
@@ -1,6 +1,7 @@
 export enum LINKS {
   DESIGN = 'https://github.com/tact-lang/docs/blob/main/tact-design.md',
   DOCS = 'https://docs.tact-lang.org',
+  PHILOSOPHY = 'https://github.com/tact-lang/tact#10-commandments-of-tact',
   GITHUB = 'https://github.com/tact-lang',
   TELEGRAM = 'https://t.me/tactlang',
   TON = 'https://ton.org/',
