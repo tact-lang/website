4ecf9aecfbb2587b6a77830588ec76ee43bbac53
Novus Nota <68142933+novusnota@users.noreply.github.com>
2025-01-31 19:34:55 +0100

    chore: partially get rid of editor components
    Removes the obsolete Tact vs. FunC side-by-side code comparison

--- src/app/features/main/main.module.ts
+++ src/app/features/main/main.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from '@features/main/main-routing.module';
-import { EditorService } from '@features/main/services/editor.service';
 import { SharedModule } from '@shared/shared.module';
 import { TuiLetModule } from '@taiga-ui/cdk';
 import { TuiDropdownControllerModule, TuiHostedDropdownModule } from '@taiga-ui/core';
 import { TuiDropdownHoverModule } from '@taiga-ui/kit';
 import { MainPageComponent } from 'src/app/features/main/main-page.component';
 import { MainPageTitleComponent } from './components/main-page-title/main-page-title.component';
-import { EditorsComponent } from './components/editors/editors.component';
-import { LabeledEditorComponent } from 'src/app/features/main/components/labeled-editor/labeled-editor.component';
 import { CodeExamplesSelectComponent } from './components/code-examples-select/code-examples-select.component';
 import { WhatIsTactComponent } from './components/what-is-tact/what-is-tact.component';
 import { CompilationErrorComponent } from './components/compilation-error/compilation-error.component';
@@ -18,8 +15,6 @@ import { CompilationErrorComponent } from './components/compilation-error/compil
   declarations: [
     MainPageComponent,
     MainPageTitleComponent,
-    EditorsComponent,
-    LabeledEditorComponent,
     CodeExamplesSelectComponent,
     WhatIsTactComponent,
     CompilationErrorComponent
@@ -33,6 +28,5 @@ import { CompilationErrorComponent } from './components/compilation-error/compil
     TuiLetModule,
     TuiDropdownControllerModule
   ],
-  providers: [EditorService]
 })
 export class MainModule {}
