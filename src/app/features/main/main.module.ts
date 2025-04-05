278378df6827d6bdb57224517fabfa52a6819864
Novus Nota <68142933+novusnota@users.noreply.github.com>
2025-04-05 14:22:38 +0200

    feat: tact contracts usage statistics and some projects that use tact contracts

--- src/app/features/main/main.module.ts
+++ src/app/features/main/main.module.ts
@@ -3,21 +3,17 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from '@features/main/main-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { TuiLetModule } from '@taiga-ui/cdk';
-import { TuiDropdownControllerModule, TuiHostedDropdownModule } from '@taiga-ui/core';
-import { TuiDropdownHoverModule } from '@taiga-ui/kit';
+import { TuiDropdownControllerModule, TuiHostedDropdownModule, TuiSvgModule } from '@taiga-ui/core';
+import { TuiDropdownHoverModule, TuiIslandModule } from '@taiga-ui/kit';
 import { MainPageComponent } from 'src/app/features/main/main-page.component';
 import { MainPageTitleComponent } from './components/main-page-title/main-page-title.component';
-import { CodeExamplesSelectComponent } from './components/code-examples-select/code-examples-select.component';
 import { WhatIsTactComponent } from './components/what-is-tact/what-is-tact.component';
-import { CompilationErrorComponent } from './components/compilation-error/compilation-error.component';
 
 @NgModule({
   declarations: [
     MainPageComponent,
     MainPageTitleComponent,
-    CodeExamplesSelectComponent,
     WhatIsTactComponent,
-    CompilationErrorComponent
   ],
   imports: [
     CommonModule,
@@ -26,7 +22,9 @@ import { CompilationErrorComponent } from './components/compilation-error/compil
     TuiHostedDropdownModule,
     TuiDropdownHoverModule,
     TuiLetModule,
+    TuiSvgModule,
+    TuiIslandModule,
     TuiDropdownControllerModule
-  ],
+  ]
 })
 export class MainModule {}
